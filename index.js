75a0cc277f89ac6c4478cf6409c4a1019ea2fcef
S-Karthick-Raja <91246056+S-Karthick-Raja@users.noreply.github.com>
2022-04-05 19:56:03 +0530

    created

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postsRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("File has been Uploaded");
+});
+
+app.get("/", (req, res) => {
+  res.status(200).send({ Msg: "Hello Welcome" });
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postsRoute);
+app.use("/api/categories", categoryRoute);
+
+app.listen(5000, () => {
+  console.log("Server is running ....");
+});
